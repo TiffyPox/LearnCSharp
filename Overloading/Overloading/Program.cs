439e9daeb41955ab16121ddfad42e5ed458631df
Tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2021-10-04 11:44:29 +0100

    added overloading file

---
+++ Overloading/Overloading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Overloading
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var v1 = new Vector(1, 2, 3);
+            var v2 = new Vector(3, 3, 3);
+            
+            var result1 = (v1 + v2); // Using the operator overload here
+            
+            var result2 = (v1 - v2);
+            
+            var v = new Vector(2, 0, -4);
+            var result3 = -v;
+
+            var result4 = (v1 * 4);
+
+            var result5 = (new Vector(2, 4, 6) / 2);
+            
+            Console.Write(result1.ToString());
+            Console.Write("\n" + result2.ToString());
+            Console.Write("\n" + result3.ToString());
+            Console.Write("\n" + result4.ToString());
+            Console.Write("\n" + result5.ToString());
+        }
+    }
+}
---
+++ Overloading/Overloading/Vector.cs
@@ -0,0 +1,50 @@
+﻿using System.Numerics;
+
+namespace Overloading
+{
+    public class Vector
+    {
+        private double X { get; set; }
+        private double Y { get; set; }
+        private double Z { get; set; }
+
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        // Add two 3D vectors together
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+
+        // Subtract one 3D vector from another
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+
+        // Negate a 3D vector
+        public static Vector operator -(Vector v)
+        {
+            return new Vector(-v.X, v.Y, -v.Z);
+        }
+
+        // Multiply a vector by a number (scaler)
+        public static Vector operator *(Vector v, double scaler)
+        {
+            return new Vector(v.X * scaler, v.Y * scaler, v.Z * scaler);
+        }
+
+        // Divide a vector by a number (scaler)
+        public static Vector operator /(Vector v, double scaler)
+        {
+            return new Vector(v.X / scaler, v.Y / scaler, v.Z / scaler);
+        }
+
+        public override string ToString() => $"X: {X} Y: {Y} Z: {Z}";
+    }
+}
