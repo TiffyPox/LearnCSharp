c97fdee43a0904da77218621b723e78c3d810101
Tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2021-10-05 11:37:13 +0100

    added dictionary

---
+++ Indexers/Dictionary.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Indexers
+{
+    public class Dictionary
+    {
+        private readonly List<string> _names = new List<string>();
+        private readonly List<string> _definitions = new List<string>();
+
+        public string this[string name]
+        {
+            get
+            {
+                var indexOf = _names.IndexOf(name);
+                if (indexOf != -1)
+                {
+                    return _definitions[indexOf];
+                }
+
+                return "The key does not exist";
+            }
+            set
+            {
+                var indexOf = _names.IndexOf(name);
+                if (indexOf == -1)
+                {
+                    _names.Add(name);
+                    _definitions.Add(value);
+                }
+                else
+                {
+                    _definitions[indexOf] = value;
+                }
+            }
+        }
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary dictionary = new Dictionary();
+            
+            dictionary["Apple"] = "A food";
+
+            dictionary["Apple"] = "A piece of fruit";
+
+            Console.WriteLine(dictionary["Apple"]);
+
+        }
+    }
+}
