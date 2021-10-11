ab754f89c316a0dcd0ddc3f7f8e3596c5f507c4e
Tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2021-10-11 09:46:47 +0100

    add Lambda Expressions

---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaExpressions
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            // To create a Lambda Expression that determines if a variable is even or odd, we would write the following code...
+            // x => x % 2 == 0
+            // The Lambda Operator (=>) is read as "goes to" or "arrow" 
+            // To read this code out loud, you would say, "x goes to x mod 2 equals 0"
+            
+            List<int> numbers = new List<int>() {1, 7, 4, 2, 5, 3, 9, 8, 6};
+            
+            numbers.Sort();
+
+            static IEnumerable<int> FindEvenNumbers(List<int> numbers)
+            {
+                return numbers.Where(x => x % 2 == 0); // Using our Lambda Expression here
+            }
+            
+            Console.WriteLine("The even numbers are:");
+            
+            foreach(var number in FindEvenNumbers(numbers))
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
